ff94db77ccedb369fe5e8e85de03ed0968b77bc9
daniel.calancea@faf.utm.md <daniel.calancea@faf.utm.md>
2016-11-12 14:08:40 +0200

    changes

--- routes/upload.js
+++ routes/upload.js
@@ -1,29 +1,54 @@
-const router = require('express').Router();
-var multer  =   require('multer');
-var app         =   express();S
-var storage =   multer.diskStorage({
-  destination: function (req, file, callback) {
-    callback(null, './uploads');
-  },
-  filename: function (req, file, callback) {
-    callback(null, file.fieldname + '-' + Date.now());
+var http = require('http');
+var router = require('routes')();
+var Busboy = require('busboy');
+var port = 5000;
+
+// Define our route for uploading files
+router.addRoute('/images', function (req, res, params) {
+  if (req.method === 'POST') {
+
+    // Create an Busyboy instance passing the HTTP Request headers.
+    var busboy = new Busboy({ headers: req.headers });
+
+    // Listen for event when Busboy finds a file to stream.
+    busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
+
+      // We are streaming! Handle chunks
+      file.on('data', function (data) {
+        // Here we can act on the data chunks streamed.
+      });
+
+      // Completed streaming the file.
+      file.on('end', function () {
+        console.log('Finished with ' + fieldname);
+      });
+    });
+
+    // Listen for event when Busboy finds a non-file field.
+    busboy.on('field', function (fieldname, val) {
+      // Do something with non-file field.
+    });
+
+    // Listen for event when Busboy is finished parsing the form.
+    busboy.on('finish', function () {
+      res.statusCode = 200;
+      res.end();
+    });
+
+    // Pipe the HTTP Request into Busboy.
+    req.pipe(busboy);
   }
 });
-var upload = multer({ storage : storage}).single('userPhoto');
 
-app.get('/',function(req,res){
-      res.sendFile(__dirname + "/index.html");
+var server = http.createServer(function (req, res) {
+
+  // Check if the HTTP Request URL matches on of our routes.
+  var match = router.match(req.url);
+
+  // We have a match!
+  if (match) match.fn(req, res, match.params);
 });
 
-app.post('/api/photo',function(req,res){
-    upload(req,res,function(err) {
-        if(err) {
-            return res.end("Error uploading file.");
-        }
-        res.end("File is uploaded");
-    });
-});
-
-app.listen(3000,function(){
-    console.log("Working on port 3000");
+server.listen(port, function () {
+  console.log('Listening on port ' + port);
 });
